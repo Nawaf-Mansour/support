0dbbe0df7661b3a9e3d09492f1b0f6c695139ffa
Nawaf Mansour <nawaf.mansour2015@gmail.com>
2019-05-26 23:09:41 +0300

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active');
+        $('ul').toggleClass('active');
+    });
+});
+
+$(document).ready(function(){
+    $('.toggle4').click(function(){
+        $('.toggle4').toggleClass('active');
+        $('ul').toggleClass('active');
+    });
+});
